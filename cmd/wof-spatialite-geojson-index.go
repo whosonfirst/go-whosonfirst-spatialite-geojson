5680cdad43e20f9843ff73fb8d90dfe0e7fd386b
thisisaaronland <straup+github@gmail.com>
2018-04-13 13:39:46 -0700

    add an is-wof flag

--- cmd/wof-spatialite-geojson-index.go
+++ cmd/wof-spatialite-geojson-index.go
@@ -9,6 +9,7 @@ import (
 	"github.com/whosonfirst/go-whosonfirst-spatialite-geojson/tables"
 	"github.com/whosonfirst/go-whosonfirst-sqlite"
 	"github.com/whosonfirst/go-whosonfirst-sqlite/database"
+	sql_index "github.com/whosonfirst/go-whosonfirst-sqlite/index"
 	"io"
 	"os"
 	"runtime"
@@ -29,6 +30,8 @@ func main() {
 	live_hard := flag.Bool("live-hard-die-fast", true, "Enable various performance-related pragmas at the expense of possible (unlikely) database corruption")
 	timings := flag.Bool("timings", false, "Display timings during and after indexing")
 	var procs = flag.Int("processes", (runtime.NumCPU() * 2), "The number of concurrent processes to index data with")
+
+	is_wof := flag.Bool("is-wof", true, "...")
 
 	flag.Parse()
 
@@ -73,10 +76,27 @@ func main() {
 		logger.Fatal("You forgot to specify which (any) tables to index")
 	}
 
-	idx, err := index.NewDefaultSQLiteFeaturesIndexer(db, to_index)
+	var idx *sql_index.SQLiteIndexer
 
-	if err != nil {
-		logger.Fatal("failed to create sqlite indexer because %s", err)
+	if *is_wof {
+
+		i, err := index.NewSpatialiteWOFIndexer(db, to_index)
+
+		if err != nil {
+			logger.Fatal("failed to create sqlite indexer because %s", err)
+		}
+
+		idx = i
+
+	} else {
+
+		i, err := index.NewSpatialiteGeoJSONIndexer(db, to_index)
+
+		if err != nil {
+			logger.Fatal("failed to create sqlite indexer because %s", err)
+		}
+
+		idx = i
 	}
 
 	idx.Timings = *timings
--- index/index.go
+++ index/index.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/whosonfirst/go-whosonfirst-geojson-v2/feature"
 	wof_index "github.com/whosonfirst/go-whosonfirst-index"
+	wof_utils "github.com/whosonfirst/go-whosonfirst-index/utils"
 	"github.com/whosonfirst/go-whosonfirst-sqlite"
 	sql_index "github.com/whosonfirst/go-whosonfirst-sqlite/index"
 	"github.com/whosonfirst/warning"
@@ -29,7 +30,7 @@ func (c Closer) Close() error {
 	return nil
 }
 
-func NewDefaultSQLiteFeaturesIndexer(db sqlite.Database, to_index []sqlite.Table) (*sql_index.SQLiteIndexer, error) {
+func NewSpatialiteGeoJSONIndexer(db sqlite.Database, to_index []sqlite.Table) (*sql_index.SQLiteIndexer, error) {
 
 	cb := func(ctx context.Context, fh io.Reader, args ...interface{}) (interface{}, error) {
 
@@ -54,6 +55,54 @@ func NewDefaultSQLiteFeaturesIndexer(db sqlite.Database, to_index []sqlite.Table
 			closer := Closer{fh}
 
 			i, err := feature.LoadFeatureFromReader(closer)
+
+			if err != nil {
+
+				msg := fmt.Sprintf("Unable to load %s, because %s", path, err)
+
+				if warning.IsWarning(err) {
+					log.Println(err)
+				} else {
+					return nil, errors.New(msg)
+				}
+			}
+
+			return i, nil
+		}
+	}
+
+	return sql_index.NewSQLiteIndexer(db, to_index, cb)
+}
+
+func NewSpatialiteWOFIndexer(db sqlite.Database, to_index []sqlite.Table) (*sql_index.SQLiteIndexer, error) {
+
+	cb := func(ctx context.Context, fh io.Reader, args ...interface{}) (interface{}, error) {
+
+		select {
+
+		case <-ctx.Done():
+			return nil, nil
+		default:
+			path, err := wof_index.PathForContext(ctx)
+
+			if err != nil {
+				return nil, err
+			}
+
+			ok, err := wof_utils.IsPrincipalWOFRecord(fh, ctx)
+
+			if err != nil {
+				return nil, err
+			}
+
+			if !ok {
+				return nil, nil
+			}
+
+			// HACK - see above
+			closer := Closer{fh}
+
+			i, err := feature.LoadWOFFeatureFromReader(closer)
 
 			if err != nil {
 
